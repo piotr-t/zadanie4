1399d882ab17e5b175af232382d15ec4139117bb
piotr-t <troc.piotr@gmail.com>
2019-04-14 20:10:42 +0200

    zadanie4

---
+++ app.js
@@ -0,0 +1,16 @@
+// 4) Given two strings, write a program that outputs the shortest sequence of character 
+// insertions and deletions that turn one string into the other.
+
+class TransformText {
+    constructor(text1, text2) {
+        this.text1 = text1;
+        this.text2 = text2;
+        this.text = [...this.text2].reduce((prev, curr, index) => {
+            console.log("  index:", index, "  prev:", prev, "  curr:", curr);
+            return curr === prev[index] ? prev : prev.substring(0, index) + this.text2[index] + prev.substr(index);
+        }, this.text1).substring(0, text2.length);
+    }
+}
+
+let newText = new TransformText("zając", "królik");
+console.log(newText);
